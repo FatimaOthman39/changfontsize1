bd1783ae0e99ba4cf5350f6d0b282ba1e195060b
FatimaOthman39 <fatimaothmannn6@gmail.com>
2024-09-25 18:28:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function ChangeSize () {
+    document.querySelector("p").setAttribute("style","font-size: 50px")
+}
+
+document.querySelector(".increase").onclick = ChangeSize
+
+
+
+
+function Font (){
+
+    document.querySelector("p").setAttribute("style","font-size: 10px")
+}
+
+document.querySelector(".decrease").onclick = Font
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
